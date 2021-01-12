c6f84a68b15bf1814b851b3adae1dbf4642ff177
Alena Dikovitska <alena.dikovitska@gmail.com>
2021-01-12 16:43:32 +0300

    MVP (model view presenter)

--- app/src/main/java/com/example/registration/LoginBusinessLogic.kt
+++ app/src/main/java/com/example/registration/LoginBusinessLogic.kt
@@ -1,5 +1,13 @@
 package com.example.registration
 
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.lang.ref.WeakReference
+
 interface LoginPresenter {
     fun login(email: String, password: String)
 }
@@ -7,4 +15,53 @@ interface LoginPresenter {
 interface LoginView {
     fun showSuccess()
     fun showError(message: String)
+    fun showError(message: Int)
+}
+
+class LoginPresenterImpl: LoginPresenter {
+
+    private var authRepository = AuthRepositoryImpl()
+    private var viewState: WeakReference<LoginView>? = null
+
+    fun attachView(view: LoginView) {
+        viewState = WeakReference(view)
+    }
+
+    override fun login(email: String, password: String) {
+
+        if(!validateEmail(email = email)) {
+            viewState?.get()?.showError(R.string.error_email_invalid)
+            return
+        }
+
+        if(!validatePassword(password = password)) {
+            viewState?.get()?.showError(R.string.error_password_invalid)
+            return
+        }
+
+        //Internal logic
+        CoroutineScope(Dispatchers.IO).async {
+
+            val errorMessage = authRepository.login(email = email, password = password).await()
+
+            if (errorMessage.isEmpty()) {
+                launch(Dispatchers.Main) {
+                    viewState?.get()?.showSuccess()
+                }
+            } else {
+                launch(Dispatchers.Main) {
+                    viewState?.get()?.showError(message = errorMessage)
+                }
+            }
+        }
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return email.contains("@") && email.contains(".")
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        return password.length > 7
+    }
+
 }
